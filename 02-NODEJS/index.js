6e3018b18a2377b491f41f8e4a72163fed0cd8be
Nilson Alcides <nilson_alcides@hotmail.com>
2019-06-17 15:39:09 -0300

    todos arquivos

---
+++ 02-NODEJS/index.js
@@ -0,0 +1,34 @@
+/*
+0 Obter um usuario
+1 Obter o numero de telefone de usuario a partir de seu Id
+2 Obter o nendereço do usuario pelo Id
+*/
+
+function obterUsuario(){
+ setTimeout(function () {
+ return{
+    id: 1,
+    nome: 'Nilton',
+    dataNascimento: new Date()
+ }
+ }, 1000)
+}
+
+function obterTelefone(IdUsuario) {
+    
+}
+
+function obterEndereco(IdUsuario) {
+    
+}
+const usuario = obterUsuario()
+const telefone = obterTelefone(usuario.id)      
+
+console.log('usuario', usuario)
+console.log('telefone', telefone)
+
+/*
+0 Obter um usuario
+1 Obter o numero de telefone de usuario a partir de seu Id
+2 Obter o nendereço do usuario pelo Id
+*/

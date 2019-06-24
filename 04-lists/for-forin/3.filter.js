4d85c3d511e9f3c386d49492600616fdbc7d736b
Nilson Alcides <nilson_alcides@hotmail.com>
2019-06-24 11:01:43 -0300

    reduce/Filter
    Filter arrayReduce

--- 04-lists/for-forin/3.filter.js
+++ 04-lists/for-forin/3.filter.js
@@ -1,6 +1,55 @@
-const {obterPessoas} = require('./servic')
+
+const { obterPessoas } = require('./service')
 
 /*
+const item = {
+    nome: 'Nilson'
+    idade: 12
+}
 
+const {nome, idade} =item
+console,log(nome,idade)
 
 */
+Array.prototype.meuFilter = function (callback){
+    const lista = []
+    for (index in this){
+        const item = this[index]
+        const result = callback(item, index, this)
+        // 0, "", null, undefined === false
+        if(!result) continue;
+        lista.push(item)
+    }
+    return lista;
+}
+
+
+async function main() {
+
+    try {
+        const {
+            results       
+        } = await obterPessoas(`a`)
+
+        // const familiaLars = results.filter(function (item){
+        //     // Por padrao precisaretornar um boleano
+        //     // Para informaaaar se deve manter ou remover daaa lista
+        //     // False > remove da lista
+        //     // true > mantem na lista
+            
+        //     const result = item.name.toLowerCase().indexOf(`lars`) !== -1
+        //     return result
+        // }) 
+        const familiaLars = results.meuFilter((item, index, lista) => {
+            console.log(`index: ${index}`, lista.length)
+            return item.name.toLowerCase().indexOf(`lars`)!== -1
+        }) 
+        
+        const names = familiaLars.map((pessoa) =>pessoa.name)
+        console.log(names)
+
+    } catch (error) {
+        console.error(`Deu ruim`, error)
+    }
+}
+main()

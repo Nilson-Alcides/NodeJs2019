cc96da3e6df7e3097c2608c5e784e81c3246889e
Nilson Alcides <nilson_alcides@hotmail.com>
2019-06-25 17:14:55 -0300

    Arquivos - READ
    Manipulando arquivivos

---
+++ 06-cli/test.js
@@ -0,0 +1,17 @@
+const {
+    deepEqual,
+    ok
+} = require('assert')
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+describe('Suite de manipulação de Heróis', () =>{
+    it('Deve cadastrar um heroi, usando arquivo', async() => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        //
+        ok(null, expected)
+    })
+})
